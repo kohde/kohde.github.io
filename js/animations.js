09ce87c05aed5b38ef668e8880c536bd588513c0
Jaco van Schalkwyk <Jaco van Schalkwyk>
2017-02-23 16:12:12 +0200

    Commiting temp site

---
+++ js/animations.js
@@ -0,0 +1,7 @@
+(function($) {
+    var header = $('#logo');
+    
+    //TweenLite.to(header, 1, {opacity: 1, y: 50}); 
+    TweenMax.to(header, 1, {scaleX:0.7, scaleY:0.7, opacity:1});
+    
+})(jQuery);
