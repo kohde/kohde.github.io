e98d0b56d23b007451becab2d7e2f77939e48895
Jaco van Schalkwyk <Jaco van Schalkwyk>
2017-03-08 15:22:26 +0200

    Initial commit

--- js/animations.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-    var header = $('#logo');
-    
-    //TweenLite.to(header, 1, {opacity: 1, y: 50}); 
-    TweenMax.to(header, 1, {scaleX:0.7, scaleY:0.7, opacity:1});
-    
-})(jQuery);
